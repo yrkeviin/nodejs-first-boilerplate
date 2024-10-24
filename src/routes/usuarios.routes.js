3741ef46adb2e19f51d5c0206cfc9c8250dd241e
Kevin Lima <kevineziquiellima160@gmail.com>
2024-10-24 11:40:25 -0300

    Feat: Create get all users

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -4,4 +4,14 @@ const usuariosRoutes = Router();
 
 let usuarios = [];
 
+usuariosRoutes.get("/", (req, res) => {
+    return res.status(200).json({
+        message:
+            usuarios.length == 0
+            ? "Não há usuários cadastrados"
+            : `Total de usuários: ${usuarios.length}`,
+        usuarios,
+    });
+});
+
 export default usuariosRoutes;
